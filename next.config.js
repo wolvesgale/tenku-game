8d4536e728837a0f061a9a70a2467349a36cb3c1
TENKU GAMES <dev@tenku-game.jp>
2026-03-01 00:58:10 +0900

    fix: add serverComponentsExternalPackages for neon

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@neondatabase/serverless"],
+  },
+};
 
 module.exports = nextConfig;
